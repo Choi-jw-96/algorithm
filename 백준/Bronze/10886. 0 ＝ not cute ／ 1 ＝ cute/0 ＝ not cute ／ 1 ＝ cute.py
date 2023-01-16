4304fd6bbd1d608b9f0784b290be56fa6ecc0670
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-01-16 15:09:19 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
@@ -0,0 +1,9 @@
+peo = int(input())
+Peo = []
+for P in range(peo):
+    Peo.append(int(input()))
+if Peo.count(1) > Peo.count(0):
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")
+    
