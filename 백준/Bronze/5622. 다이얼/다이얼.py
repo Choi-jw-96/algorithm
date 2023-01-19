aabb93a4cbd0f868694b1ad73b0d9c0fb6d14258
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-01-19 15:17:45 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,11 @@
+tel = {2 : "ABC", 3 : "DEF", 4 : "GHI", 5 : "JKL", 
+6 : "MNO", 7 : "PQRS", 8 : "TUV", 9 : "WXYZ"}
+
+phone = input()
+time = 0
+
+for P in phone:
+    for num, alpha in tel.items():
+        if P in alpha:
+            time += num + 1
+print(time)
