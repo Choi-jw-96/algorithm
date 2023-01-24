4fae9d91369b8c8fdb16c38e33b5080000431075
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-01-24 16:36:08 +0900

    [Silver V] Title: 셀프 넘버, Time: 48 ms, Memory: 31636 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,14 +1,11 @@
-not_self_num = []
-self_num = []
-
-for n in range(1, 10001):
+remove_set = set()
+for n in set(range(1, 10001)):
     for n_1 in str(n):
         n += int(n_1)
     if n <= 10000:
-        not_self_num.append(n)
+        remove_set.add(n)
 
-for n in range(1, 10001):
-    if n not in not_self_num:
-        self_num.append(n)
+self_num = set(range(1, 10001)) - remove_set
+print(*sorted(self_num), sep = "\n", )
 
-print(*self_num, sep = "\n")
+
