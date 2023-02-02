9560ebcfdb606f6831ad0f5a15e6970b2da92dd4
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-02-03 03:12:57 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,24 +1,19 @@
+T = int(input())
 cnt = 0
 
-
-T = int(input())
-
 for t in range(T):
-    str_input = input()
-    str_list = []
-    n = 0
-    for s_1 in str_input:       
-        if s_1 not in str_list:
-            str_list.append(s_1)
-        
-    for s_1 in str_list:        
-        if str_input.count(s_1) == 1:
-            n += 1
-        
+    word = input()
+    word_dic = {}
+    line_word = ""
+    for s in word:
+        if s not in word_dic:
+            word_dic[s] = 1
         else:
-            if str_input[n:n + str_input.count(s_1)] == s_1 * str_input.count(s_1):
-                n += str_input.count(s_1)
-            else:
-                cnt += 1
-                break
-print(T - cnt)
+            word_dic[s] += 1
+    
+    for K, V in word_dic.items():
+        line_word += K * V
+    
+    if line_word == word:
+        cnt += 1
+print(cnt)
