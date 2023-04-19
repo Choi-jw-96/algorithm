e71fac4ab86860f67e0cd6622bd3087a447d691a
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-04-19 15:45:24 +0900

    [Silver III] Title: 피보나치 함수, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,14 @@
+def fibonacci(n) : 
+    if n >= 3 :
+        for i in range(2, n) :
+            fibonacci_0.append(fibonacci_0[i-1]+fibonacci_0[i])
+            fibonacci_1.append(fibonacci_1[i-1]+fibonacci_1[i])
+    print(fibonacci_0[n],fibonacci_1[n])
+
+T = int(input())
+
+for t in range(T) :
+    fibonacci_0 = [1,0,1]
+    fibonacci_1 = [0,1,1]
+    n = int(input()) 
+    fibonacci(n)
