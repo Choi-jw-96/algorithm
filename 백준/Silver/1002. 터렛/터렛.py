320c9872474026119c9d6e842f3561436ee6c787
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-06-26 22:19:34 +0900

    [Silver III] Title: 터렛, Time: 64 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,15 +1,22 @@
+# 두 원의 중심 반지름을 입력했을 때 접점을 구하는 문제
+# 공식이 필요한 문제
 import math
 
-n = int(input())
-
-for _ in range(n):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)  # 두 원의 거리 (원의방정식활용)
-    if distance == 0 and r1 == r2 :  # 두 원이 동심원이고 반지름이 같을 때
-        print(-1)
-    elif abs(r1-r2) == distance or r1 + r2 == distance:  # 내접, 외접일 때
-        print(1)
-    elif abs(r1-r2) < distance < (r1+r2) :  # 두 원이 서로다른 두 점에서 만날 때
-        print(2)
+for i in range(int(input())):
+    jx, jy, r1, bx, by, r2= map(int, input().split())
+    # math.pow(x, y) 함수는 x의 y 거듭제곱 (x의 y승)을 반환 (x가 음수 y가 실수면 error)
+    # math.sqrt(x) 함수는 x의 제곱근을 반환 (음수가 나오면 error)
+    distance = math.sqrt((jx - bx) ** 2 + (jy - by)**2)
+    # 원의 중심이 같을때
+    if distance == 0:
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
     else:
-        print(0)  # 그 외에
+        if r1 + r2 == distance or abs(r2 - r1) == distance:
+            print(1)
+        elif (abs(r2 - r1) < distance and (distance < r1 + r2)):
+            print(2)
+        else:
+            print(0)
